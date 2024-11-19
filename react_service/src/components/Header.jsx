adb1eb4cd5641a83af539dfc9be17142eb1c6f71
ssdeok <bechu123456@gmail.com>
2024-11-19 14:49:29 +0900

    practice second

--- react_service/src/components/Header.jsx
+++ react_service/src/components/Header.jsx
@@ -5,12 +5,11 @@ export default function Header() {
   return (
     <header>
       <ul>
-        <h3>출발비디오여행</h3>
         <li>
           <Link to="/">Home으로</Link>
         </li>
         <li>
-          <Link to="/posts">마이페이지</Link>
+          <Link to="/posts">게시글로</Link>
         </li>
       </ul>
     </header>
